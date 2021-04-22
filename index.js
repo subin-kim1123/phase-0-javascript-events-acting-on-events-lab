260777543167c9d808feca61f5707d7dfd0258c7
Subin Kim <https://github.com/subin-kim1123>
2021-04-21 21:38:44 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+document.querySelector("html")// Your code here
+
+const dodger = document.getElementById("dodger"); 
+
+document.addEventListener("keydown", function(event) {
+    console.log(event);
+  });
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+}); 
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.right.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+  
+    if (right > 0) {
+      dodger.style.right = `${left + 1}px`;
+    }
+} 
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+});
+
+
+
+
